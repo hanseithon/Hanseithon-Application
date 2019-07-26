76a6b37cd8c956efecf72bd8bfe0ca5f195c469b
B1ns <qlsl3574@naver.com>
2019-07-26 11:37:53 +0900

    Change Code

--- app/src/main/java/com/b1ns/hanseithon/view/main/MainActivity.kt
+++ app/src/main/java/com/b1ns/hanseithon/view/main/MainActivity.kt
@@ -2,20 +2,17 @@ package com.b1ns.hanseithon.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import com.b1ns.hanseithon.R
+import com.b1ns.hanseithon.SettingActivity
 import com.b1ns.hanseithon.SnsActivity
-import com.b1ns.hanseithon.data.Board
-import com.b1ns.hanseithon.data.ResultBoard
-import com.b1ns.hanseithon.data.ResultLogin
-import com.b1ns.hanseithon.utils.Utils
-import com.b1ns.hanseithon.view.user.LoginActivity
+import com.google.android.material.navigation.NavigationView
+
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
-import retrofit2.Call
-import javax.security.auth.callback.Callback
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,5 +26,37 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener {
             startActivity<SnsActivity>()
         }
+
+        menu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+
     }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){ // 네비게이션 메뉴가 클릭되면 스낵바가 나타난다.
+            R.id.reword -> {
+
+            }
+            R.id.setting -> {
+                startActivity<SettingActivity>()
+            }
+            R.id.userSetting ->{
+
+            }
+         }
+        drawerLayout.closeDrawers() // 기능을 수행하고 네비게이션을 닫아준다.
+        return false
+    }
+
+
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawers()
+        }else{
+            super.onBackPressed()
+        }
+    }
+
 }
